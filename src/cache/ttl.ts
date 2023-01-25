91fd6771263bd1af2295b94af99a50eb1b345afa
Jim Chen <yijunc@andrew.cmu.edu>
2023-01-25 18:40:37 -0500

    translate ttl.js into typescript

--- src/cache/ttl.ts
+++ src/cache/ttl.ts
@@ -1,13 +1,37 @@
-"use strict";
-const TTLCache = require("@isaacs/ttlcache");
-const pubsub = require("../pubsub");
-module.exports = function (opts) {
-    const ttlCache = new TTLCache(opts);
-    const cache = {};
+import TTLCache = require('@isaacs/ttlcache');
+import pubsub = require('../pubsub');
+
+interface Options{
+    name?: string,
+    ttl?: number,
+    enabled?: boolean,
+}
+
+interface Cache{
+    name?: string,
+    hits: number,
+    misses: number,
+    enabled: boolean,
+    set: (key: string, value: number, ttl: number) => void,
+    get: (key: string) => number,
+    del: (keys: string[]) => void,
+    delete: (keys: string[]) => void,
+    reset: () => void,
+    clear: () => void,
+    getUnCachedKeys: (keys: string[], cachedData: Record<string, number>) => string[]
+    dump: () => [string, number][],
+    peek: (key: string) => number,
+}
+
+export = function (opts: Options) {
+    const ttlCache = new TTLCache<string, number>(opts);
+
+    const cache = {} as Cache;
     cache.name = opts.name;
     cache.hits = 0;
     cache.misses = 0;
     cache.enabled = opts.enabled === undefined ? false : opts.enabled;
+
     // expose properties
     const propertyMap = new Map([
         ['max', 'max'],
@@ -18,22 +42,24 @@ module.exports = function (opts) {
     propertyMap.forEach((ttlProp, cacheProp) => {
         Object.defineProperty(cache, cacheProp, {
             get: function () {
-                return ttlCache[ttlProp];
+                return ttlCache[ttlProp] as number;
             },
             configurable: true,
             enumerable: true,
         });
     });
+
     cache.set = function (key, value, ttl) {
         if (!cache.enabled) {
             return;
         }
-        const opts = {};
+        const opts = {} as Options;
         if (ttl) {
             opts.ttl = ttl;
         }
         ttlCache.set(key, value, opts);
     };
+
     cache.get = function (key) {
         if (!cache.enabled) {
             return undefined;
@@ -41,12 +67,12 @@ module.exports = function (opts) {
         const data = ttlCache.get(key);
         if (data === undefined) {
             cache.misses += 1;
-        }
-        else {
+        } else {
             cache.hits += 1;
         }
         return data;
     };
+
     cache.del = function (keys) {
         if (!Array.isArray(keys)) {
             keys = [keys];
@@ -55,24 +81,29 @@ module.exports = function (opts) {
         keys.forEach(key => ttlCache.delete(key));
     };
     cache.delete = cache.del;
+
     function localReset() {
         ttlCache.clear();
         cache.hits = 0;
         cache.misses = 0;
     }
+
     cache.reset = function () {
         pubsub.publish(`${cache.name}:ttlCache:reset`);
         localReset();
     };
     cache.clear = cache.reset;
+
     pubsub.on(`${cache.name}:ttlCache:reset`, () => {
         localReset();
     });
-    pubsub.on(`${cache.name}:ttlCache:del`, (keys) => {
+
+    pubsub.on(`${cache.name}:ttlCache:del`, (keys: string[]) => {
         if (Array.isArray(keys)) {
             keys.forEach(key => ttlCache.delete(key));
         }
     });
+
     cache.getUnCachedKeys = function (keys, cachedData) {
         if (!cache.enabled) {
             return keys;
@@ -83,21 +114,25 @@ module.exports = function (opts) {
             data = cache.get(key);
             isCached = data !== undefined;
             if (isCached) {
-                cachedData[key] = data;
+                cachedData[key] = data as number;
             }
             return !isCached;
         });
+
         const hits = keys.length - unCachedKeys.length;
         const misses = keys.length - hits;
         cache.hits += hits;
         cache.misses += misses;
         return unCachedKeys;
     };
+
     cache.dump = function () {
         return Array.from(ttlCache.entries());
     };
+
     cache.peek = function (key) {
         return ttlCache.get(key, { updateAgeOnGet: false });
     };
+
     return cache;
 };
